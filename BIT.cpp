9801ac03496848b783ec19f8d58dbcf49b94e221
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-01-06 22:01:39 +0600

    Added BIT/Fenwick Tree

---
+++ BIT.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+#define faster ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
+#define read freopen("in.txt", "r", stdin)
+#define write freopen("out.txt", "w", stdout)
+#define mem(x, n) memset(x, n, sizeof(x))
+#define all(x) x.begin(), x.end()
+#define endl "\n"
+
+// Insert 0 at the begining of input array to make it 1 based indexing
+class BIT
+{
+private:
+	vector<ll> m_array;
+
+public:
+	BIT(int sz) // Max size of the array
+	{
+		m_array.resize(sz + 1, 0);
+	}
+
+	void build(vector<ll> &list)
+	{
+		for (int i = 1; i <= list.size(); i++)
+		{
+			m_array[i] = list[i];
+		}
+
+		for (int ind = 1; ind <= m_array.size(); ind++)
+		{
+			int ind2 = ind + (ind & -ind);
+			if (ind2 <= m_array.size())
+			{
+				m_array[ind2] += m_array[ind];
+			}
+		}
+	}
+
+	ll prefix_query(int ind)
+	{
+		int res = 0;
+		for (; ind > 0; ind -= (ind & -ind))
+		{
+			res += m_array[ind];
+		}
+		return res;
+	}
+
+	ll range_query(int from, int to)
+	{
+		return prefix_query(to) - prefix_query(from - 1);
+	}
+
+	void add(int ind, ll add)
+	{
+		for (; ind < m_array.size(); ind += (ind & -ind))
+		{
+			m_array[ind] += add;
+		}
+	}
+
+	void empty(int ind)
+	{
+		ll val = range_query(ind, ind);
+		add(ind, -1 * val);
+	}
+};
+
+int main()
+{
+	faster;
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+	}
+
+	return 0;
+}
