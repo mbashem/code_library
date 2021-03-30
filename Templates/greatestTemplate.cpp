c4b35861f6145ef7ee83a517470f2ead5c6a8a17
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-03-30 23:13:45 +0600

    Template updated

--- Templates/greatestTemplate.cpp
+++ Templates/greatestTemplate.cpp
@@ -3,27 +3,30 @@
 using namespace std;
 
 typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
 
 #define faster ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0)
-#define read freopen("in.txt","r",stdin)
-#define write freopen("out.txt","w",stdout)
-#define mem(x,n) memset(x,n,sizeof(x))
+#define read freopen("in.txt", "r", stdin)
+#define write freopen("out.txt", "w", stdout)
+#define var(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+#define Set(n, p) (n = (n | (1LL << p)))
+#define Reset(n, p) (n = (n & ~(1LL << p)))
+#define Check(n, p) (!((n & (1LL << p)) == 0))
+#define mem(x, n) memset(x, n, sizeof(x))
 #define all(x) x.begin(), x.end()
 #define endl "\n"
 
 int main()
 {
 	faster;
-	
+
 	int t;
 	cin >> t;
-	
-	while(t--)
+
+	while (t--)
 	{
-		
 	}
-	
+
 	return 0;
 }
