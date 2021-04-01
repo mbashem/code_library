2bec857ac57a3741b503e9f7e4cb2472d1948eca
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-04-01 10:29:31 +0600

    Modified The Greatest Template

--- Templates/greatestTemplate.cpp
+++ Templates/greatestTemplate.cpp
@@ -10,12 +10,24 @@ typedef pair<ll, ll> pll;
 #define read freopen("in.txt", "r", stdin)
 #define write freopen("out.txt", "w", stdout)
 #define var(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
-#define Set(n, p) (n = (n | (1LL << p)))
-#define Reset(n, p) (n = (n & ~(1LL << p)))
-#define Check(n, p) (!((n & (1LL << p)) == 0))
 #define mem(x, n) memset(x, n, sizeof(x))
 #define all(x) x.begin(), x.end()
 #define endl "\n"
+
+ll Set(ll n, int p)
+{
+	return n | (1LL << p);
+}
+
+ll Reset(ll n, int p)
+{
+	return n & ~(1LL << p);
+}
+
+bool Check(ll n, int p)
+{
+	return (n & (1LL << p)) != 0;
+}
 
 int main()
 {
