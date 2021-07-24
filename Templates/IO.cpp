9df9d8b7d8f7bc718a6e898916112753ffb4fe2d
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-07-24 10:26:01 +0600

    io update

--- Templates/IO.cpp
+++ Templates/IO.cpp
@@ -6,112 +6,134 @@ typedef long long ll;
 typedef pair<int,int> pii;
 typedef pair<ll,ll> pll;
 
-#define faster ios_base::sync_with_stdio(false), cin.tie(0)
-#define read freopen("in.txt","r",stdin)
-#define write freopen("out.txt","w",stdout)
 #define var(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
 #define mem(x,n) memset(x,n,sizeof(x))
 #define all(x) x.begin(), x.end()
-#define endl "\n"
 
-struct IO
+namespace ios
 {
-	inline static const string ALONE = "", SPACE = " ", ENDL = "\n";
+#define dbg(...) print(#__VA_ARGS__, __VA_ARGS__)
+	static const char sp = ' ', en = '\n', com = ',';
+	static const string line = "\n###################\n";
+	bool debug;
 
-	IO(bool fast = true)
+	auto flush() -> void
 	{
-		if (fast)
-			ios_base::sync_with_stdio(false), cin.tie(0);
-		setPrecision(10);
+		cout << std::flush;
 	}
-
-	IO(string in, string out, bool fast = false) : IO(fast)
-	{
-		freopen((in + ".in").c_str(), "r", stdin);
-		freopen((out + ".out").c_str(), "w", stdout);
-	}
-
-	IO(string in, bool fast = false) : IO(in, in, fast) {}
 
 	auto setPrecision(int precision) -> void
 	{
 		cout << fixed << setprecision(precision);
 	}
 
-	template <typename T>
-	auto read(T &x)
+	auto IO(bool fast = true)
 	{
+		debug = !fast;
+		if (fast)
+			ios_base::sync_with_stdio(false), cin.tie(0);
+		setPrecision(10);
+	}
+
+	auto IO(string in, string write, bool fast = true)
+	{
+		IO(fast);
+		if (fast)
+		{
+			freopen((in + ".in").c_str(), "r", stdin);
+			freopen((write + ".write").c_str(), "w", stdout);
+		}
+	}
+
+	auto IO(string in, bool fast = true)
+	{
+		IO(in, in, fast);
+	}
+
+	template <typename T>
+	auto read(T &x) -> bool
+	{
+		if (cin.eof())
+			return false;
 		cin >> x;
-	}
 
-	auto endl()
-	{
-		cout << "\n";
-	}
-
-	auto flush()
-	{
-		cout << std::flush;
-	}
-
-	auto line()
-	{
-		cout << "\n###################\n";
-	}
-
-	// template <typename T>
-	// auto write(string &x, string en = ALONE, bool flush = false)
-	// {
-	// 	cout << x << en;
-	// 	if (flush)
-	// 		cout << std::flush;
-	// }
-
-	template <typename T>
-	auto write(T x, string en = ALONE, bool flush = false)
-	{
-		cout << x << en;
-		if (flush)
-			cout << std::flush;
+		return true;
 	}
 
 	template <typename T, typename... Args>
-	auto write(T &x, Args... args)
+	auto read(T &x, Args &...args) -> bool
 	{
-		write(x);
-		write(args...);
-		cout << std::flush;
+		return read(x) && read(args...);
 	}
 
 	template <typename ITER>
-	auto writeIt(ITER begin, ITER end, string en = ALONE, string IterEn = ENDL, bool flus = false)
+	auto readIt(ITER begin, ITER end) -> void
 	{
 		while (begin != end)
 		{
-			write(*begin, en);
+			read(*begin);
 			begin++;
 		}
+	}
 
-		cout << IterEn;
+	auto ln() -> void
+	{
+		cout << ios::line;
+	}
 
-		if (flus)
+	template <typename T>
+	auto write(T x) -> void
+	{
+		cout << x;
+	}
+
+	template <typename T, typename... Args>
+	auto write(T x, Args... args) -> void
+	{
+		write(x);
+		write(args...);
+	}
+
+	template <typename ITER>
+	auto writeIt(ITER begin, ITER end, string between = "") -> void
+	{
+		while (begin != end)
 		{
-			flush();
+			write(*begin);
+			write(between);
+			begin++;
 		}
+	}
+
+	// debug
+	template <typename Arg1>
+	auto print(const char *name, Arg1 &&arg1) -> void
+	{
+		if (!debug)
+			return;
+		if (name[0] == ' ')
+			name++;
+		cerr << "[" << name << ": " << arg1 << "]"
+				 << "\n";
+	}
+
+	template <typename Arg1, typename... Args>
+	auto print(const char *names, Arg1 &&arg1, Args &&...args) -> void
+	{
+		if (!debug)
+			return;
+		const char *comma = strchr(names + 1, ',');
+		cerr << "[";
+		cerr.write(names, comma - names) << ": " << arg1 << "] ";
+		print(comma + 1, args...);
 	}
 };
 
-template <class... Args>
-void print(Args... args)
-{
-	(std::cout << ... << args) << "\n";
-}
+using namespace ios;
 
 
 int main()
 {
-	faster;
-	
 	int t;
 	cin >> t;
 	
