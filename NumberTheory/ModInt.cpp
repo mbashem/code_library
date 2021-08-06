18a54c7190b3c2f3cc8b87aca80a6e2b5163b301
Mohammed Bashem <45259719+mbashem@users.noreply.github.com>
2021-08-06 23:26:09 +0600

    modint

--- NumberTheory/ModInt.cpp
+++ NumberTheory/ModInt.cpp
@@ -15,127 +15,65 @@ typedef pair<ll, ll> pll;
 #define endl "\n"
 
 /*Copied from neals submisson :https://codeforces.com/contest/1536/submission/118611234 */
+
 template <const int &MOD>
-struct _m_int
+struct modInt
 {
 	int val;
 
-	_m_int(int64_t v = 0)
+	modInt(ll v = 0)
 	{
 		if (v < 0)
 			v = v % MOD + MOD;
 		if (v >= MOD)
 			v %= MOD;
-		val = int(v);
+		val = v;
 	}
 
-	_m_int(uint64_t v)
+	modInt &operator+=(const modInt &a)
 	{
-		if (v >= MOD)
-			v %= MOD;
-		val = int(v);
-	}
-
-	_m_int(int v) : _m_int(int64_t(v)) {}
-	_m_int(unsigned v) : _m_int(uint64_t(v)) {}
-
-	explicit operator int() const { return val; }
-	explicit operator unsigned() const { return val; }
-	explicit operator int64_t() const { return val; }
-	explicit operator uint64_t() const { return val; }
-	explicit operator double() const { return val; }
-	explicit operator long double() const { return val; }
-
-	_m_int &operator+=(const _m_int &other)
-	{
-		val -= MOD - other.val;
+		val -= MOD - a.val;
 		if (val < 0)
 			val += MOD;
 		return *this;
 	}
 
-	_m_int &operator-=(const _m_int &other)
+	modInt &operator-=(const modInt &a)
 	{
-		val -= other.val;
+		val -= a.val;
 		if (val < 0)
 			val += MOD;
 		return *this;
 	}
 
-	static unsigned fast_mod(uint64_t x, unsigned m = MOD)
+	modInt &operator*=(const modInt &a)
 	{
-#if !defined(_WIN32) || defined(_WIN64)
-		return unsigned(x % m);
-#endif
-		// Optimized mod for Codeforces 32-bit machines.
-		// x must be less than 2^32 * m for this to work, so that x / m fits in an unsigned 32-bit int.
-		unsigned x_high = unsigned(x >> 32), x_low = unsigned(x);
-		unsigned quot, rem;
-		asm("divl %4\n"
-				: "=a"(quot), "=d"(rem)
-				: "d"(x_high), "a"(x_low), "r"(m));
-		return rem;
-	}
-
-	_m_int &operator*=(const _m_int &other)
-	{
-		val = fast_mod(uint64_t(val) * other.val);
+		val = (uint64_t(val) * a.val) % MOD;
 		return *this;
 	}
 
-	_m_int &operator/=(const _m_int &other)
+	modInt &operator/=(const modInt &a)
 	{
-		return *this *= other.inv();
+		return *this *= a.inv();
 	}
 
-	friend _m_int operator+(const _m_int &a, const _m_int &b) { return _m_int(a) += b; }
-	friend _m_int operator-(const _m_int &a, const _m_int &b) { return _m_int(a) -= b; }
-	friend _m_int operator*(const _m_int &a, const _m_int &b) { return _m_int(a) *= b; }
-	friend _m_int operator/(const _m_int &a, const _m_int &b) { return _m_int(a) /= b; }
+	friend modInt operator+(const modInt &a, const modInt &b) { return modInt(a) += b; }
+	friend modInt operator-(const modInt &a, const modInt &b) { return modInt(a) -= b; }
+	friend modInt operator*(const modInt &a, const modInt &b) { return modInt(a) *= b; }
+	friend modInt operator/(const modInt &a, const modInt &b) { return modInt(a) /= b; }
 
-	_m_int &operator++()
-	{
-		val = val == MOD - 1 ? 0 : val + 1;
-		return *this;
-	}
-
-	_m_int &operator--()
-	{
-		val = val == 0 ? MOD - 1 : val - 1;
-		return *this;
-	}
-
-	_m_int operator++(int)
-	{
-		_m_int before = *this;
-		++*this;
-		return before;
-	}
-	_m_int operator--(int)
-	{
-		_m_int before = *this;
-		--*this;
-		return before;
-	}
-
-	_m_int operator-() const
-	{
-		return val == 0 ? 0 : MOD - val;
-	}
-
-	friend bool operator==(const _m_int &a, const _m_int &b) { return a.val == b.val; }
-	friend bool operator!=(const _m_int &a, const _m_int &b) { return a.val != b.val; }
-	friend bool operator<(const _m_int &a, const _m_int &b) { return a.val < b.val; }
-	friend bool operator>(const _m_int &a, const _m_int &b) { return a.val > b.val; }
-	friend bool operator<=(const _m_int &a, const _m_int &b) { return a.val <= b.val; }
-	friend bool operator>=(const _m_int &a, const _m_int &b) { return a.val >= b.val; }
+	friend bool operator==(const modInt &a, const modInt &b) { return a.val == b.val; }
+	friend bool operator!=(const modInt &a, const modInt &b) { return a.val != b.val; }
+	friend bool operator<(const modInt &a, const modInt &b) { return a.val < b.val; }
+	friend bool operator>(const modInt &a, const modInt &b) { return a.val > b.val; }
+	friend bool operator<=(const modInt &a, const modInt &b) { return a.val <= b.val; }
+	friend bool operator>=(const modInt &a, const modInt &b) { return a.val >= b.val; }
 
 	static const int SAVE_INV = int(1e6) + 5;
-	static _m_int save_inv[SAVE_INV];
+	static modInt save_inv[SAVE_INV];
 
 	static void prepare_inv()
 	{
-		// Make sure MOD is prime, which is necessary for the inverse algorithm below.
 		for (int64_t p = 2; p * p <= MOD; p += p % 2 + 1)
 			assert(MOD % p != 0);
 
@@ -146,7 +84,7 @@ struct _m_int
 			save_inv[i] = save_inv[MOD % i] * (MOD - MOD / i);
 	}
 
-	_m_int inv() const
+	modInt inv() const
 	{
 		if (save_inv[1] == 0)
 			prepare_inv();
@@ -154,7 +92,7 @@ struct _m_int
 		if (val < SAVE_INV)
 			return save_inv[val];
 
-		_m_int product = 1;
+		modInt product = 1;
 		int v = val;
 
 		while (v >= SAVE_INV)
@@ -166,49 +104,31 @@ struct _m_int
 		return product * save_inv[v];
 	}
 
-	_m_int pow(int64_t p) const
+	friend ostream &operator<<(ostream &out, const modInt &m)
 	{
-		if (p < 0)
-			return inv().pow(-p);
-
-		_m_int a = *this, result = 1;
-
-		while (p > 0)
-		{
-			if (p & 1)
-				result *= a;
-
-			p >>= 1;
-
-			if (p > 0)
-				a *= a;
-		}
-
-		return result;
+		out << m.val;
+		return out;
 	}
 
-	friend ostream &operator<<(ostream &os, const _m_int &m)
+	friend istream &operator>>(istream &in, modInt &m)
 	{
-		return os << m.val;
+		in >> m.val;
+		return in;
 	}
 };
 
-template <const int &MOD>
-_m_int<MOD> _m_int<MOD>::save_inv[_m_int<MOD>::SAVE_INV];
-
-extern const int MOD = int(1e9) + 7;
-using mint = _m_int<MOD>;
+const int MOD = 1e9 + 7;
+template<const int &MOD> modInt<MOD> modInt<MOD>::save_inv[modInt<MOD>::SAVE_INV];
+using mint = modInt<MOD>;
 
 int main()
 {
 	faster;
 
-	int t;
-	cin >> t;
-
-	while (t--)
-	{
-	}
+	mint a = 50000000000000000ULL;
+	a += 6;
+	a/=2;
+	cout << a << endl;
 
 	return 0;
 }
